69d1b1285761118d927951b3162653dae1155ece
ryhux <59020801+ryhux@users.noreply.github.com>
2021-02-01 21:28:55 +0300

    Add files via upload

---
+++ Ryhux/index.php
@@ -0,0 +1,46 @@
+﻿<?php
+$a = "aa";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ryhux</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/ico">
+	<link rel="stylesheet" type="text/css" href="indexstyles.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="scripts/index.js"></script>
+	<meta charset="utf-8">
+</head>
+<body>
+	<div class="header"><a id="logo">Ryhux</a></div>
+	<div class="data">
+		
+		<div class="main">
+			<div class="left_block"><a href="">
+				<ul>
+					<li><div><a class="menuitem" href=""><img src="feed.svg" width="24" height="24">Лента</a></div></li>
+					<li><div><a class="menuitem" href=""><img src="feed.svg" width="24" height="24">Мой профиль</a></div></li>
+				</ul>
+			</a>
+			</div>
+			<div class="center_block">
+				<div class="content">
+					<div>texttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttt</div>
+					<div>texttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttttexttextetxtetxttetxtettxttttttttttttttttt</div>
+					
+				</div>
+				<div class="footer">
+					<div>ryhux.github.io 2019-2021</div>
+				</div>
+				
+				
+			</div>
+
+		</div>
+		
+	</div>
+	
+	
+</body>
+</html>
